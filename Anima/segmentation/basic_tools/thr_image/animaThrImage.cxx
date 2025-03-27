b9e8cba7ba87f27d29315240137252884ae5d1ea
Gregoire Ville <gregoire.ville@inria.fr>
2025-03-27 11:13:22 +0100

    First modifications to make the feature generalizable to 2D and 4D images

--- Anima/segmentation/basic_tools/thr_image/animaThrImage.cxx
+++ Anima/segmentation/basic_tools/thr_image/animaThrImage.cxx
@@ -11,62 +11,48 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
+template<int nbDim>
+int createOutputMaskImage(string inputImageName, string inputMaskImageName, string outputImageName, double thrValue, double upperThr, double adaptThr, bool outputValuesInversion)
 {
-    TCLAP::CmdLine cmd("INRIA / IRISA - VisAGeS/Empenn Team", ' ',ANIMA_VERSION);
+    typedef itk::Image<double, nbDim> DoubleImageType;
+    typedef itk::Image<unsigned char, nbDim> UCImageType;
     
-    TCLAP::ValueArg<std::string> inputArg("i","inputimage","Input image",true,"","Input image",cmd);
-    TCLAP::ValueArg<std::string> outputArg("o","outputimage","Output image",true,"","Output image",cmd);
-    TCLAP::ValueArg<std::string> maskArg("m","maskfile","mask file",false,"","mask file",cmd);
+    typedef itk::ImageRegionConstIterator<DoubleImageType> DoubleImageIterator;
+    typedef itk::ImageRegionIterator<UCImageType> UCImageIterator;
 
-    TCLAP::ValueArg<double> thrArg("t","thr","Threshold value",false,1.0,"Threshold value",cmd);
-    TCLAP::ValueArg<double> upperThrArg("u","uthr","Upper threshold value",false,std::numeric_limits <double>::max(),"Upper threshold value",cmd);
-    TCLAP::ValueArg<double> adaptThrArg("a","adaptivethr","Adaptative threshold value (between 0 and 1)",false,0.0,"adaptative threshold value",cmd);
+    typedef itk::ThresholdImageFilter<DoubleImageType> ThresholdFilterType;
+    typedef itk::ThresholdLabelerImageFilter<DoubleImageType, UCImageType> LabelerFilterType;
+
+    typename DoubleImageType::Pointer inputImage = anima::readImage <DoubleImageType>(inputImageName);
     
-    TCLAP::SwitchArg invArg("I","inv","Computes 1-res",cmd,false);
-    
-    try
+    typename DoubleImageType::RegionType tmpRegionInputImage = inputImage->GetLargestPossibleRegion();
+    DoubleImageIterator doubleIt(inputImage, tmpRegionInputImage);
+
+    unsigned int totalSize = 1;
+
+    for (unsigned int i = 0; i < nbDim; i++)
     {
-        cmd.parse(argc,argv);
-    }
-    catch (TCLAP::ArgException& e)
-    {
-        std::cerr << "Error: " << e.error() << "for argument " << e.argId() << std::endl;
-        return EXIT_FAILURE;
+        totalSize *= tmpRegionInputImage.GetSize()[i];
     }
 
-    typedef itk::Image<double,3> DoubleImageType;
-    typedef itk::Image<unsigned char, 3> UCImageType;
-    
-    typedef itk::ImageRegionConstIterator <DoubleImageType> DoubleImageIterator;
-    typedef itk::ImageRegionIterator <UCImageType> UCImageIterator;
-
-    typedef itk::ThresholdImageFilter <DoubleImageType> ThresholdFilterType;
-    typedef itk::ThresholdLabelerImageFilter <DoubleImageType,UCImageType> LabelerFilterType;
-
-    DoubleImageType::Pointer inputImage = anima::readImage <DoubleImageType> (inputArg.getValue());
-    
-    DoubleImageType::RegionType tmpRegionInputImage = inputImage->GetLargestPossibleRegion();
-    DoubleImageIterator doubleIt(inputImage,tmpRegionInputImage);
-
-    unsigned int totalSize = tmpRegionInputImage.GetSize()[0]*tmpRegionInputImage.GetSize()[1]*tmpRegionInputImage.GetSize()[2];
-    std::vector <double> tmpVec (totalSize);
+    //std::vector<double> tmpVec(totalSize);
 
     unsigned int idx=0;
-    
-    if (maskArg.getValue() != "")
+    /*
+    if (inputMaskImageName != "")
     {
-        UCImageType::Pointer maskImage = anima::readImage <UCImageType> (maskArg.getValue());
+        typename UCImageType::Pointer maskImage = anima::readImage<UCImageType>(inputMaskImageName);
 
-        UCImageType::RegionType tmpRegionMaskImage = maskImage->GetLargestPossibleRegion();
+        typename UCImageType::RegionType tmpRegionMaskImage = maskImage->GetLargestPossibleRegion();
         UCImageIterator ucIt(maskImage,tmpRegionMaskImage);
 
-        if ((tmpRegionInputImage.GetSize()[0]!=tmpRegionMaskImage.GetSize()[0]) ||
-                (tmpRegionInputImage.GetSize()[1]!=tmpRegionMaskImage.GetSize()[1]) ||
-                (tmpRegionInputImage.GetSize()[2]!=tmpRegionMaskImage.GetSize()[2]))
+        for (unsigned int i = 0; i < nbDim; i++)
         {
-            std::cerr << "InputImage size != MaskImage size" << std::endl;
-            return EXIT_FAILURE;
+            if (tmpRegionInputImage.GetSize()[i]!=tmpRegionMaskImage.GetSize()[i])
+            {
+                std::cerr << "InputImage size != MaskImage size" << std::endl;
+                return EXIT_FAILURE;
+            }
         }
 
         for (unsigned int i = 0;i < totalSize;++i)
@@ -92,35 +78,34 @@ int main(int argc, char **argv)
             ++doubleIt;
         }
     }
+        */
 
-    if ((adaptThrArg.getValue() < 0) || (adaptThrArg.getValue() > 1))
+    if ((adaptThr < 0) || (adaptThr > 1))
     {
         std::cerr << "Adaptative threshold value has to be included in the [0,1] interval" << std::endl;
         return EXIT_FAILURE;
     }
     
-    unsigned int partialElt = (unsigned int)floor(adaptThrArg.getValue()*idx);
+    unsigned int partialElt = (unsigned int) floor(adaptThr*idx);
     if (partialElt == idx)
         partialElt = idx - 1;
 
-    if (partialElt != 0)
-        std::partial_sort(tmpVec.begin(),tmpVec.begin() + partialElt + 1,tmpVec.end());
+    //if (partialElt != 0)
+        //std::partial_sort(tmpVec.begin(),tmpVec.begin() + partialElt + 1,tmpVec.end());
 
-    double thrV = thrArg.getValue();
-    if (partialElt != 0)
-        thrV = tmpVec[partialElt];
+    //if (partialElt != 0)
+        //thrValue = tmpVec[partialElt];
 
-    ThresholdFilterType::Pointer thrFilter = ThresholdFilterType::New();
+    typename ThresholdFilterType::Pointer thrFilter = ThresholdFilterType::New();
     thrFilter->SetInput(inputImage);
 
-    double upperThr = upperThrArg.getValue();
-    if (upperThr < thrV)
-        upperThr = thrV;
+    if (upperThr < thrValue)
+        upperThr = thrValue;
 
     if (upperThr != USHRT_MAX)
-        thrFilter->ThresholdOutside(thrV,upperThr);
+        thrFilter->ThresholdOutside(thrValue,upperThr);
     else
-        thrFilter->ThresholdBelow(thrV);
+        thrFilter->ThresholdBelow(thrValue);
     
     try
     {
@@ -132,10 +117,10 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
     
-    LabelerFilterType::Pointer mainFilter = LabelerFilterType::New();
+    typename LabelerFilterType::Pointer mainFilter = LabelerFilterType::New();
     mainFilter->SetInput(thrFilter->GetOutput());
 
-    LabelerFilterType::RealThresholdVector thrVals;
+    typename LabelerFilterType::RealThresholdVector thrVals;
     thrVals.push_back(0);
 
     mainFilter->SetRealThresholds(thrVals);
@@ -150,9 +135,9 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
     
-    if (invArg.isSet())
+    if (outputValuesInversion)
     {
-        UCImageIterator resIt(mainFilter->GetOutput(),tmpRegionInputImage);
+        UCImageIterator resIt(mainFilter->GetOutput(), tmpRegionInputImage);
         
         for (unsigned int i = 0;i < totalSize;++i)
         {
@@ -161,7 +146,214 @@ int main(int argc, char **argv)
         }
     }
 
-    anima::writeImage <UCImageType> (outputArg.getValue(),mainFilter->GetOutput());
-
+    anima::writeImage <UCImageType> (outputImageName, mainFilter->GetOutput());
     return EXIT_SUCCESS;
 }
+
+
+
+
+
+//using x1 = template<int nbCompenent>
+
+
+
+int main(int argc, char **argv)
+{
+    TCLAP::CmdLine cmd("INRIA / IRISA - VisAGeS/Empenn Team", ' ',ANIMA_VERSION);
+    
+    TCLAP::ValueArg<std::string> inputArg("i","inputimage","Input image",true,"","Input image",cmd);
+    TCLAP::ValueArg<std::string> outputArg("o","outputimage","Output image",true,"","Output image",cmd);
+    TCLAP::ValueArg<std::string> maskArg("m","maskfile","mask file",false,"","mask file",cmd);
+
+    TCLAP::ValueArg<double> thrArg("t","thr","Threshold value",false,1.0,"Threshold value",cmd);
+    TCLAP::ValueArg<double> upperThrArg("u","uthr","Upper threshold value",false,std::numeric_limits <double>::max(),"Upper threshold value",cmd);
+    TCLAP::ValueArg<double> adaptThrArg("a","adaptivethr","Adaptative threshold value (between 0 and 1)",false,0.0,"adaptative threshold value",cmd);
+    
+    TCLAP::SwitchArg invArg("I","inv","Computes 1-res",cmd,false);
+    
+    try
+    {
+        cmd.parse(argc,argv);
+    }
+    catch (TCLAP::ArgException& e)
+    {
+        std::cerr << "Error: " << e.error() << "for argument " << e.argId() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    itk::ImageIOBase::Pointer imageIO = itk::ImageIOFactory::CreateImageIO(inputArg.getValue().c_str(), itk::IOFileModeEnum::ReadMode);
+    imageIO->SetFileName(inputArg.getValue());
+    imageIO->ReadImageInformation();
+
+    unsigned int nbComponents = imageIO->GetNumberOfComponents();
+    unsigned int nbDim = imageIO->GetNumberOfDimensions();
+
+    int exit_value; 
+
+    switch (nbDim)
+    {
+        case 2:
+            exit_value = createOutputMaskImage<2>(inputArg.getValue(), maskArg.getValue(), outputArg.getValue(), thrArg.getValue(), upperThrArg.getValue(), adaptThrArg.getValue(), invArg.isSet());
+            break;
+        case 3:
+            exit_value = createOutputMaskImage<3>(inputArg.getValue(), maskArg.getValue(), outputArg.getValue(), thrArg.getValue(), upperThrArg.getValue(), adaptThrArg.getValue(), invArg.isSet());
+            break;
+        case 4:
+            exit_value = createOutputMaskImage<4>(inputArg.getValue(), maskArg.getValue(), outputArg.getValue(), thrArg.getValue(), upperThrArg.getValue(), adaptThrArg.getValue(), invArg.isSet());
+            break;
+        default:
+            std::cerr<<"Unsupported number of dimensions for input Image (supported values are : 2, 3 and 4)"<<std::endl;
+            break;
+    }
+
+    return exit_value;
+}
+
+    /*
+    if (nbComponents == 1)
+    {
+        //scalar image
+    
+    }
+
+    else
+    {
+        //vectorial image
+    }
+        */
+
+    //typedef itk::Image<double, nbDim> DoubleImageType;
+    //typedef itk::Image<unsigned char, nbDim> UCImageType;
+    //
+    //typedef itk::ImageRegionConstIterator <DoubleImageType> DoubleImageIterator;
+    //typedef itk::ImageRegionIterator <UCImageType> UCImageIterator;
+//
+    //typedef itk::ThresholdImageFilter <DoubleImageType> ThresholdFilterType;
+    //typedef itk::ThresholdLabelerImageFilter <DoubleImageType, UCImageType> LabelerFilterType;
+//
+    //DoubleImageType::Pointer inputImage = anima::readImage <DoubleImageType> (inputArg.getValue());
+    //
+    //DoubleImageType::RegionType tmpRegionInputImage = inputImage->GetLargestPossibleRegion();
+    //DoubleImageIterator doubleIt(inputImage, tmpRegionInputImage);
+//
+    //for (unsigned int i = 0; i < nbDim; i++)
+    //{
+    //    totalSize *= tmpRegionInputImage.GetSize()[i];
+    //}
+//
+    //std::vector<double> tmpVec(totalSize);
+//
+    //unsigned int idx=0;
+    //
+    //if (maskArg.getValue() != "")
+    //{
+    //    UCImageType::Pointer maskImage = anima::readImage <UCImageType> (maskArg.getValue());
+//
+    //    UCImageType::RegionType tmpRegionMaskImage = maskImage->GetLargestPossibleRegion();
+    //    UCImageIterator ucIt(maskImage,tmpRegionMaskImage);
+//
+    //    for (unsigned int i = 0; i < nbDim; i++)
+    //    {
+    //        if (tmpRegionInputImage.GetSize()[i]!=tmpRegionMaskImage.GetSize()[i])
+    //        {
+    //            std::cerr << "InputImage size != MaskImage size" << std::endl;
+    //            return EXIT_FAILURE;
+    //        }
+    //    }
+//
+    //    for (unsigned int i = 0;i < totalSize;++i)
+    //    {
+    //        if(ucIt.Get() == 1)
+    //        {
+    //            tmpVec[idx] = doubleIt.Get();
+    //            ++idx;
+    //        }
+//
+    //        ++ucIt;
+    //        ++doubleIt;
+    //    }
+//
+    //    tmpVec.resize(idx);
+    //}
+    //else
+    //{
+    //    idx = totalSize;
+    //    for (unsigned int i = 0;i < totalSize;++i)
+    //    {
+    //        tmpVec[i] = doubleIt.Get();
+    //        ++doubleIt;
+    //    }
+    //}
+//
+    //if ((adaptThrArg.getValue() < 0) || (adaptThrArg.getValue() > 1))
+    //{
+    //    std::cerr << "Adaptative threshold value has to be included in the [0,1] interval" << std::endl;
+    //    return EXIT_FAILURE;
+    //}
+    //
+    //unsigned int partialElt = (unsigned int) floor(adaptThrArg.getValue()*idx);
+    //if (partialElt == idx)
+    //    partialElt = idx - 1;
+//
+    //if (partialElt != 0)
+    //    std::partial_sort(tmpVec.begin(),tmpVec.begin() + partialElt + 1,tmpVec.end());
+//
+    //double thrV = thrArg.getValue();
+    //if (partialElt != 0)
+    //    thrV = tmpVec[partialElt];
+//
+    //ThresholdFilterType::Pointer thrFilter = ThresholdFilterType::New();
+    //thrFilter->SetInput(inputImage);
+//
+    //double upperThr = upperThrArg.getValue();
+    //if (upperThr < thrV)
+    //    upperThr = thrV;
+//
+    //if (upperThr != USHRT_MAX)
+    //    thrFilter->ThresholdOutside(thrV,upperThr);
+    //else
+    //    thrFilter->ThresholdBelow(thrV);
+    //
+    //try
+    //{
+    //    thrFilter->Update();
+    //}
+    //catch (itk::ExceptionObject &e)
+    //{
+    //    std::cerr << e << std::endl;
+    //    return EXIT_FAILURE;
+    //}
+    //
+    //LabelerFilterType::Pointer mainFilter = LabelerFilterType::New();
+    //mainFilter->SetInput(thrFilter->GetOutput());
+//
+    //LabelerFilterType::RealThresholdVector thrVals;
+    //thrVals.push_back(0);
+//
+    //mainFilter->SetRealThresholds(thrVals);
+//
+    //try
+    //{
+    //    mainFilter->Update();
+    //}
+    //catch (itk::ExceptionObject &e)
+    //{
+    //    std::cerr << e << std::endl;
+    //    return EXIT_FAILURE;
+    //}
+    //
+    //if (invArg.isSet())
+    //{
+    //    UCImageIterator resIt(mainFilter->GetOutput(), tmpRegionInputImage);
+    //    
+    //    for (unsigned int i = 0;i < totalSize;++i)
+    //    {
+    //        resIt.Set(1-resIt.Get());
+    //        ++resIt;
+    //    }
+    //}
+//
+    //anima::writeImage <UCImageType> (outputArg.getValue(), mainFilter->GetOutput());
+//
+    
